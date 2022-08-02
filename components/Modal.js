97e41fe178854e9096642772b24ceb28ea7caf11
AlexisF9 <alexis.flacher@tbwa-paris.com>
2022-08-02 17:16:08 +0200

    delete modifs

--- components/Modal.js
+++ components/Modal.js
@@ -26,32 +26,26 @@ export default function Modal({ rea, open, setModal }) {
       >
         close
       </button>
-      <div className="p-0 w-[90%]">
+      <div className="p-0 w-[90%] md:w-[80%]">
         <h2 className="text-start md:text-center font-[NewYork] text-6xl h-[30vh] md:h-[25vh] flex md:justify-center items-center">
           {rea.title}
         </h2>
-        <div className="md:flex">
-          {rea.picture ? (
-            <Slider pictures={rea.picture} />
-          ) : (
-            <p>Chargement des images...</p>
-          )}
-          <div className="pl-0 md:pl-10">
-            <p className="md:my-0 my-10 md:min-w-[30%]">{rea.description}</p>
+        <div>
+          <Slider pictures={rea.picture} />
+          <p className="mt-10 md:min-w-[30%]">{rea.description}</p>
 
-            <ul className="flex md:mt-8 mt-0">
-              {rea.tags.map((item, i) => {
-                return (
-                  <li
-                    className="mr-2 border border-black rounded-md w-max px-2 py-1"
-                    key={i}
-                  >
-                    {item}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+          <ul className="flex mt-5 mb-20">
+            {rea.tags.map((item, i) => {
+              return (
+                <li
+                  className="mr-2 border border-black rounded-md w-max px-2 py-1"
+                  key={i}
+                >
+                  {item}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </div>
