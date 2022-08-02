ce31b3f559c95fdc30edb66d3cf325a95eec8935
AlexisF9 <alexis.flacher@tbwa-paris.com>
2022-08-02 15:19:01 +0200

    add data in json

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Home({ realisations }) {
 
       <div
         ref={intro}
-        className="relative opacity-0 w-full w-[70%] min-h-full flex flex-col bg-white justify-center items-center text-center"
+        className="relative opacity-0 w-full md:w-[70%] min-h-full flex flex-col bg-white justify-center items-center text-center"
       >
         <div ref={introContent} className="opacity-0">
           <h1 className="font-[NewYork] text-7xl md:text-7xl lg:text-8xl">
